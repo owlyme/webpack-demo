dc35af080233e3f1153d5777e36321d116c1d93a
owlyme <chudequan_48@163.com>
2018-05-09 12:59:57 +0800

    asset-manager-config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Data from './data.xml';
-import MyImage from './my-image.png';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-  var myimg = new Image();
-  myimg.src = MyImage;
-
-  element.appendChild(myimg);
-  
-  console.log(Data)
   return element;
 }
 
