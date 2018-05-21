16d8b70d5b8e1fe69db8389b5cf4895e0f73476e
owlyme <chudequan_48@163.com>
2018-05-21 19:17:22 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 import printme from "./print.js";
+import { cube } from "./math.js";
 
+import "../asset/style.css";
 
 function component() {
   // var element = document.createElement("div");
   var element = document.getElementById("container");
+  
   var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', cube(5)], ' ');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printme;
 
@@ -18,3 +21,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,21 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  // mode: "production",
   entry: {
     app: "./src/index.js"
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module:{
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
